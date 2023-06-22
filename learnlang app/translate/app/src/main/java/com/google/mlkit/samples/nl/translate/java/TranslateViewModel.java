859c0fdc4c10bafc37d227c3df557b1e6785f960
Un_Chained <91795469+Sid-dhant@users.noreply.github.com>
2023-06-22 14:23:17 +0530

    Update 1

--- learnlang app/translate/app/src/main/java/com/google/mlkit/samples/nl/translate/java/TranslateViewModel.java
+++ learnlang app/translate/app/src/main/java/com/google/mlkit/samples/nl/translate/java/TranslateViewModel.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package com.google.mlkit.samples.nl.translate.java;
 
 import android.app.Application;
@@ -44,9 +28,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-/**
- * Model class for tracking available models and performing live translations
- */
+
 public class TranslateViewModel extends AndroidViewModel {
   // This specifies the number of translators instance we want to keep in our LRU cache.
   // Each instance of the translator is built with different options based on the source
